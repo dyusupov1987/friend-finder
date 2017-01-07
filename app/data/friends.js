a5938b03551a190f05eff08b10ce9efc7cf8d844
dyusupov1987 <Dysny.diliyor@gmail.com>
2017-01-06 19:29:30 -0500

    final commit

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+//array that holds all the 'friends' that take the survey (initially filled with dummy data)
+var friendList = [
+  {
+    name: 'Muhammet',
+    profilePic: 'http://lorempixel.com/400/200/',
+    scores: ['5','5','5','5','5','5','5','5','5','5']
+  }, {
+    name: 'Rando',
+    profilePic: 'http://lorempixel.com/400/200/',
+    scores: ['3','3','3','3','3','3','3','3','3','3']
+  }, {
+    name: 'El Chapo',
+    profilePic: 'http://lorempixel.com/400/200/',
+    scores: ['1','1','1','1','1','1','1','1','1','1']
+  }
+]
+
+
+module.exports = friendList;  
